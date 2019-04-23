80af60c8bdd28266997e17c1d2a317b136ac6879
Bernard Huff <bernard.huff008@gmail.com>
2019-04-23 11:14:54 -0500

    Sweep

--- page.php
+++ page.php
@@ -19,17 +19,23 @@ while (have_posts()) {
 
         <!-- Here is the Meta-Box -->
 
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title( ); ?></span></p>
-        </div>
+        <?php
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        if ($theParent) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            </div>
+        <?php }
+    ?>
+
 
         <!-- <div class="page-links">
-                <h2 class="page-links__title"><a href="#">About Us</a></h2>
-                <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
-                </ul>
-            </div> -->
+                    <h2 class="page-links__title"><a href="#">About Us</a></h2>
+                    <ul class="min-list">
+                        <li class="current_page_item"><a href="#">Our History</a></li>
+                        <li><a href="#">Our Goals</a></li>
+                    </ul>
+                </div> -->
 
         <div class="generic-content">
             <?php the_content(); ?>
