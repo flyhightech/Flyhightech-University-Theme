565503793712a0aa84eb549e868dede9ad34b5d7
Bernard Huff <bernard.huff008@gmail.com>
2019-10-14 15:50:50 -0500

    Refactoring the code

--- header.php
+++ header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?> >
 
 <head>
+    <meta charset="<?php bloginfo('charset'); ?>" >
+    <meta name="viewport" content="width=device-width initial-scale=1">
     <?php wp_head(); ?>
-    <title>Flyhightech University</title>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Flyhightech</strong> University</a></h1>
